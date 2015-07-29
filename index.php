a3e0ded7ae2cca6fb84a10db70737ada71187d44
Nikolas Martens <nikolas.martens@researchgate.net>
2015-07-29 17:56:47 +0200

    With JS again

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
+<?php
+    $name = "World";
+?>
 <html>
 <head>
     <title>Test page</title>
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script>
+        $(function () {
+            $('body').append('<h1>Hello <?php echo $name ?>!</h1>');
+        });
+    </script>
 </head>
 
-<body>
-
-<h1>Hello World</h1>
-
-</body>
+<body></body>
 </html>
--- test.php
+++ test.php
@@ -7,7 +7,7 @@ require_once 'vendor/autoload.php';
 
 echo "Test started" . PHP_EOL;
 
-$driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', [WebDriverCapabilityType::BROWSER_NAME => 'phantomjs']);
+$driver = RemoteWebDriver::create('http://127.0.0.1:4444/wd/hub', [WebDriverCapabilityType::BROWSER_NAME => 'phantomjs']);
 
 $driver->get('http://127.0.0.1:8000/index.php');
 
